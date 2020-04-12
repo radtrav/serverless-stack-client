da818f9194f48b68f708ed968dce4542aeed0f20
Travis Young <travis.young@telus.com>
2020-04-12 19:49:15 -0400

    feat(spinner): add in spinner when logging in

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./Routes";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { Auth } from "aws-amplify";
 import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { AppContext } from "./libs/contextLib";
+import { onError } from "./libs/errorLib";
 import "./App.css";
 
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
 
-  function handleLogout() {
+  const history = useHistory();
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        onError(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
     userHasAuthenticated(false);
+    history.push("/login");
   }
 
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -38,8 +64,9 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
-      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated }}
+      >
         <Routes />
       </AppContext.Provider>
     </div>
